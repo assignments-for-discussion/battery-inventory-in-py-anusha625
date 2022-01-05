1dad734e390fc0fb2f07720ef81b48ce6725ab85
anusha625 <84381291+anusha625@users.noreply.github.com>
2022-01-05 20:17:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def test_bucketing_by_number_of_cycles():
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
   if charge < 400:
     assert(counts["lowCount"] == 2)
-  if (charge < 400 && charge < 999):
+  if (charge < 400 && charge < 919):
     assert(counts["mediumCount"] == 3)
   else:
     assert(counts["highCount"] == 1)
