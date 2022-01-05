3cafa2f66cf60c6052d1f896918776ada10d7cd3
anusha625 <84381291+anusha625@users.noreply.github.com>
2022-01-05 20:14:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ def count_batteries_by_usage(cycles):
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
   counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
+  if charge < 400:
+    assert(counts["lowCount"] == 2)
+  if (charge < 400 && charge < 999):
+    assert(counts["mediumCount"] == 3)
+  else:
+    assert(counts["highCount"] == 1)
   print("Done counting :)")
 
 
 if __name__ == '__main__':
   test_bucketing_by_number_of_cycles()
+
+  charged less than 400 times: classified as low
+charged between 400 and 919 times: classified as medium
+charged 920 times or more: classified as high
